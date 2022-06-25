e917a900dde1a25fdc6b584bc8954f47c612706c
Ezh+Wine <valvic90@gmail.com>
2022-06-25 13:20:46 +0300

    1st task completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(111, 1000);
+    }
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+Console.WriteLine("Задайте размер массива с трехзначными числами:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateRandomArray(size);
+ShowArray(array);
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+        count ++;
+}
+Console.WriteLine();
+Console.WriteLine("Четных чисел в массиве -> " + count);
